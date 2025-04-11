d2d3a044dcf1536344c259d4d76386c2cb1dc9bb
Power <powerleecn@foxmail.com>
2025-04-11 16:30:03 +0000

    推送 AirDrop 文件夹到 main 分支
    添加 Faucet 文件夹及其内容
    保持其他文件夹不变

---
+++ AirDrop/frontend/components/footer.js
@@ -0,0 +1,13 @@
+export default function Footer() {
+    return (
+      <footer className=" font-wq w-full text-blue-900 text-center mt-auto py-4 text-xl ">
+        <p>
+          本项目仅供参考，不建议使用在生产环境中！！！
+        </p>
+        <h2>
+            如果这个对你有所帮助，欢迎 github 给个星星,非常感谢！
+        </h2>
+      </footer>
+    );
+  }
+  
